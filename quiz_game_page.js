40c21fceda4b33a6a1ff8c1932ab7eab06c32104
KrishivGangwani <77724297+KrishivGangwani@users.noreply.github.com>
2021-09-10 14:36:20 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -35,22 +35,32 @@ function check(){
     get_answer = document.getElementById("input_check_box").value;
     if(get_answer == actual_answer){
         if(answer_turn == "player1"){
-            update_player1_score = player1_score + 1;
-            document.getElementById("player1_score").innerHTML = update_player1_score;
+            player1_score = player1_score + 1;
+            document.getElementById("score1").innerHTML = player1_score;
          }
          else{
-             update_player2_score = player2_score + 1;
-            document.getElementById("player2_score").innerHTML = update_player2_score;
+             player2_score = player2_score + 1;
+            document.getElementById("score2").innerHTML = player2_score;
          }
     }
 
     if(question_turn == "player1"){
         question_turn = "player2"
-        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+        document.getElementById("question").innerHTML = "Question Turn - " + player2_name;
     }   
     else{
         question_turn = "player1"
-        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+        document.getElementById("question").innerHTML = "Question Turn - " + player1_name;
+
+    }
+
+    if(answer_turn == "player1"){
+        answer_turn = "player2"
+        document.getElementById("answer").innerHTML = "Answer Turn - " + player2_name;
+    }   
+    else{
+        answer_turn = "player1"
+        document.getElementById("answer").innerHTML = "Answer Turn - " + player1_name;
 
     }
 }
